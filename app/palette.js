8984c7c844bbb39386cf39d8ee17a614266b7c3f
rdad <akindofceasar@gmail.com>
2016-10-07 16:28:05 +0200

    palette opérationnelle : select color et events

--- app/palette.js
+++ app/palette.js
@@ -2,24 +2,50 @@
 pixel_studio.palette = {
 
 	colors: [],
+	color_selected: null,
 
-	init_colors: function( color_list ){
+	select_color:  function( color ){
 
-		this.colors = color_list;
+		this.color_selected = color;
+		$('#colors li').removeClass('selected')
+						.eq(color.id)
+						.addClass('selected');
+	},
 
+	init_colors: function( colors ){
+
+		this.colors = colors;
+
+		//  création de la représentation de la palette
+		
 		let $colors = $('#colors'),
-			$one = $colors.children('li').detach();
+			$one 	= $colors.children('li').detach();
 
-		for(let j=0; j<color_list.length; j++){
+		for(let j=0; j<colors.length; j++){
 
 			let li 		= $one.clone(),
 				color 	= this.colors[j];
 
+			color.id 	= j;
+
 			li.css('background-color', color.to_string())
 			  .attr('title', color.name);
 			$colors.append(li);
-
 		}
+
+		// couleur par defaut
+		
+		this.select_color(this.colors[0]);
+
+		// gestion des click
+		
+		var self = this;
+
+		$('#colors').on('click', 'li', function(){		
+			let index = $( "#colors li" ).index( this );
+			self.select_color(self.colors[index]);
+		});
+
 		console.log('palette : colors ready');
 	}
 };
